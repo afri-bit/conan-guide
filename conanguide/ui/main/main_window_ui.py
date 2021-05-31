67fedf435934d5955675c2b50c6b610f1920b12b
Karl Wallner <kwallner@mail.de>
2021-05-31 20:44:08 +0200

    Regenerated code with pyside2 (now with from-exports argument) * No source-code changes - just regenerated things.

--- conanguide/ui/main/main_window_ui.py
+++ conanguide/ui/main/main_window_ui.py
@@ -12,7 +12,7 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
-import resources_rc
+from  . import resources_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
